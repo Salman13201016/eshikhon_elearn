86fc97f50b483a05e41f0e0d2d0492866189ffc0
John Doe <test@mail.com>
2023-09-01 20:05:13 +0600

    category inserted successfully without validation

--- elearn/elearn/views.py
+++ elearn/elearn/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, redirect
+from django.contrib import messages
+from .models import Categories
 def index(request):
     return render(request,'admin/cat.html')
 def insert(request):
     cat_name = request.POST.get('cat_name')
+
+    cat_obj = Categories()
+    cat_obj.name = cat_name
+
+    cat_obj.save()
+    messages.success(request, 'Category Inserted successfully')
+    return redirect('catadmin')
+
     
