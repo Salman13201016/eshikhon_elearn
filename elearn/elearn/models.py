e7c8dc042b25310cebe464d1f42a2ec80478b7fe
John Doe <test@mail.com>
2023-09-02 20:11:03 +0600

    category validation - empty, length, unique done

--- elearn/elearn/models.py
+++ elearn/elearn/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 class Categories(models.Model):
      id = models.AutoField(primary_key=True)
-     name = models.CharField(max_length=500)
+     name = models.CharField(max_length=500, unique=True)
--- elearn/elearn/views.py
+++ elearn/elearn/views.py
@@ -5,12 +5,28 @@ def index(request):
     return render(request,'admin/cat.html')
 def insert(request):
     cat_name = request.POST.get('cat_name')
+    if cat_name:
+        if len(cat_name)<4:
+            messages.success(request, 'the cat name field length minimum 3')
+            
+        elif Categories.objects.filter(name=cat_name).exists():
+            messages.success(request, 'This value already exists.')
+        else:
 
-    cat_obj = Categories()
-    cat_obj.name = cat_name
+            cat_obj = Categories()
+            cat_obj.name = cat_name
 
-    cat_obj.save()
-    messages.success(request, 'Category Inserted successfully')
+            cat_obj.save()
+            messages.success(request, 'Category Inserted successfully')    
+    else:
+        messages.success(request, 'The field can not be empty')
+       
+
+    # cat_obj = Categories()
+    # cat_obj.name = cat_name
+
+    # cat_obj.save()
+    # messages.success(request, 'Category Inserted successfully')
     return redirect('catadmin')
 
     
